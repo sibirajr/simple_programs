6b1df89d020370597d8974e2bf14cfda9d5e35d0
sibirajr <r.sibiraj.007@gmail.com>
2016-10-24 18:58:50 +0530

    christmas tree
    TCS interview question 2016

---
+++ christmas tree.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+int standfun();
+int initialfun();
+int loop();
+int structure(int);
+int days,initial,stand;
+int main()
+{
+	scanf("%d",&days);
+	if(days==1)
+	{
+		printf("you cannot print the tree");
+	}
+	else if(days>20)
+	{
+		printf("tree is no more");
+	}
+	else 
+	{
+	initial=days;
+	stand=days;
+	initialfun();
+	loop();
+	standfun();
+}
+return 0;
+}
+int initialfun()
+{
+	int i;
+	for(i=1;i<=initial;i++)
+	{
+		printf("\t");
+	}
+	printf("*");
+	printf("\n");
+}
+
+int standfun()
+{
+	int count,i;
+	count=2;
+	while(count)
+	{
+
+	for(i=1;i<=stand;i++)
+	{
+		printf("\t");
+	}
+	printf("*");
+	printf("\n");
+	count--;
+	}	
+}
+
+int loop()
+{
+	int count,intermediate,tab;
+	intermediate=days;
+	count=days-1;
+	while(count)
+	{
+		structure(intermediate);
+		intermediate--;
+		count--;
+	}	
+}
+int structure(argument)
+{
+	int tab,stars,i;
+	tab=days-1;
+	stars=3;
+	while(argument)
+	{
+		for(i=1;i<=tab;i++)
+		{
+			printf("\t");
+		}
+		for(i=1;i<=stars;i++)
+		{
+			printf("*\t");
+		}
+		printf("\n");
+		stars=stars+2;
+		argument--;
+		tab--;
+	}
+}
+
